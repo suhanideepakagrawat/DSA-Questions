ed948a333f9c0b103adcd2d1a5f7ef1487a2e0b7
Suhani Deepak Agrawat <147186161+suhanideepakagrawat@users.noreply.github.com>
2026-05-28 21:55:10 +0530

    Time: 8 ms (55.38%), Space: 11.5 MB (41.7%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -11,22 +11,14 @@ public:
         int minlen = INT_MAX;
 
         while (r < n) {
-            // If we find a char that is needed, increment cnt
             if (hash[s[r]] > 0) cnt++;
-            
-            // Decrement the map for the current char (even if it goes negative)
             hash[s[r]]--;
-
-            // When window contains all chars from t
             while (cnt == m) {
                 if (r - l + 1 < minlen) {
                     minlen = r - l + 1;
                     sIndex = l;
                 }
-
-                // Try to shrink from the left
                 hash[s[l]]++;
-                // If the char was essential (count becomes > 0), we broke the window
                 if (hash[s[l]] > 0) cnt--;
                 l++;
             }
