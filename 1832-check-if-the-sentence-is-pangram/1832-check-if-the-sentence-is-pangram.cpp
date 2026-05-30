ba29ad30c95783a38fcacd6e6fbd2a527d12f6d6
Suhani Deepak Agrawat <147186161+suhanideepakagrawat@users.noreply.github.com>
2026-05-31 00:08:22 +0530

    Time: 0 ms (100%), Space: 8.8 MB (83.83%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        unordered_map<char,int> hash;
-        for (int i = 0; i<sentence.length(); i++){
-            char c = sentence[i];
-            hash[c]++;
+        int count[26] = {0};
+        int uniqueLetters = 0;
+        
+        for (int i = 0; i < sentence.length(); i++) {
+            int index = sentence[i] - 'a';
+            
+            // If this is the FIRST time we are seeing this letter
+            if (count[index] == 0) {
+                uniqueLetters++;
+            }
+            
+            count[index]++;
+            
+            // Optimization: Stop immediately if we found all 26 letters!
+            if (uniqueLetters == 26) {
+                return true;
+            }
         }
-        if (hash.size() == 26){return true;}
-        return false;  
+        
+        return false;
     }
 };
