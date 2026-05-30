e081bb80c6df38e74c425a2cf1fb4c73583b2771
Suhani Deepak Agrawat <147186161+suhanideepakagrawat@users.noreply.github.com>
2026-05-30 23:45:07 +0530

    Time: 0 ms (100%), Space: 8.5 MB (78.42%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while(num >= 10){
-            int sum = 0;
-            while(num>0){
-                sum += num % 10;
-                num = num / 10;
-            }
-        num = sum;  
-        }
-    return num;   
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
     }
 };
